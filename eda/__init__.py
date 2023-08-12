9615491df13287902503c3384e314354666f5d4d
Mikhail Martin <johndoe110@yandex.ru>
2023-08-12 13:06:30 +0300

    делаю необязательным установку calplot

--- eda/__init__.py
+++ eda/__init__.py
@@ -1,13 +1,21 @@
-from ._eda_distribution import (
-    cat_feature_report,
-    num_feature_report,
-    na_bar_plot,
-)
-from ._eda_datashift import (
-    area_plot,
-    na_datashift,
-    ridge_plot,
-)
+from ._eda_distribution import cat_feature_report, num_feature_report, na_bar_plot
+from ._eda_datashift import area_plot, ridge_plot
+
+
+def unsupported_calplot(*args, **kwargs):
+    raise ImportError('Не установлен calplot. Нажми `pip install calplot`))')
+
+
+try:
+    import calplot
+    have_calplot = True
+except ImportError:
+    have_calplot = False
+
+if have_calplot:
+    from ._eda_datashift import na_datashift
+else:
+    na_datashift = unsupported_calplot
 
 __all__ = [
     # distribution
