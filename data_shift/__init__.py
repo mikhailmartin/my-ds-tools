297510bd5ea467dcc6d55f8dfe70b94df9f450fc
Mikhail Martin <johndoe110@yandex.ru>
2023-08-14 20:02:33 +0300

    calplot issue

--- data_shift/__init__.py
+++ data_shift/__init__.py
@@ -1,9 +1,21 @@
-from ..eda import (
-    ridge_plot,
-    area_plot,
-    na_datashift,
-)
+from ..eda import ridge_plot, area_plot
 from .forget_weights import get_forget_weights
+
+
+def unsupported_calplot(*args, **kwargs):
+    raise ImportError('Не установлен calplot. Нажми `pip install calplot`))')
+
+
+try:
+    import calplot
+    have_calplot = True
+except ImportError:
+    have_calplot = False
+
+if have_calplot:
+    from ..eda import na_datashift
+else:
+    na_datashift = unsupported_calplot
 
 
 __all__ = [
